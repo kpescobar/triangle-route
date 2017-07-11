bfd5826adf01390b6fb2af08c8ba505261b20702
kpescobar <kellyp.721@live.com>
2017-07-11 16:27:12 -0600

    File IO.

---
+++ src/edu/cnm/deepdive/triangle/Solver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.triangle;
+
+/**
+ * @author Kelly Escobar
+ *
+ */
+public class Solver {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    Data data = new Data("data/data.txt");
+
+  }
+
+}
